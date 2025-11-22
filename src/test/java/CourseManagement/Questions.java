c2e00af97b73c2e8a2ce245178afbeead1101d88
Ak <135825619+CozyMellow@users.noreply.github.com>
2025-11-22 22:58:45 +0200

    Added questions attributes & constructor

--- src/test/java/CourseManagement/Questions.java
+++ src/test/java/CourseManagement/Questions.java
@@ -1,4 +1,21 @@
 package CourseManagement;
 
+import java.util.ArrayList;
+
 public class Questions {
+    private String question;
+    private ArrayList<String> mcqOptions;
+    private int indexOfCorrectOption;
+
+
+
+
+    public Questions(String question, ArrayList<String> mcqOptions, int indexOfCorrectOption){
+        this.question = question;
+        this.mcqOptions = mcqOptions;
+        this.indexOfCorrectOption = indexOfCorrectOption;
+    }
+
+
 }
+
