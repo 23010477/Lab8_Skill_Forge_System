becd7f7e83f22b0ebaf7b98510bc0f7b4010dd7e
Mohamed <mohamedwalid.elkholy@gmail.com>
2025-11-22 17:37:07 +0200

    created methods

--- src/test/java/InstructorManagement/StudentPerformanceAnalytics.java
+++ src/test/java/InstructorManagement/StudentPerformanceAnalytics.java
@@ -1,13 +1,15 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package InstructorManagement;
 
-/**
- *
- * @author OMEN
- */
+package InstructorManagement;
 public class StudentPerformanceAnalytics {
+  
+    public void recordQuizResults(){
+        
+    }
+    public void lessonCompletion(){
+        
+    }
+    public void progressionData(){
+        
+    }
     
 }
