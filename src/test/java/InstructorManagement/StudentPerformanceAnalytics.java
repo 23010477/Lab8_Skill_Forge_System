c3ccc521185ab327bb09e7f50d183f09155c6b06
Mohamed <mohamedwalid.elkholy@gmail.com>
2025-11-22 18:26:55 +0200

    IMplemented progressiondata method

--- src/test/java/InstructorManagement/StudentPerformanceAnalytics.java
+++ src/test/java/InstructorManagement/StudentPerformanceAnalytics.java
@@ -1,14 +1,26 @@
 
 package InstructorManagement;
+import CourseManagement.*;
+import Student.*;
+import java.util.ArrayList;
 public class StudentPerformanceAnalytics {
   
     public void recordQuizResults(){
         
     }
-    public void lessonCompletion(){
-        
+    public void lessonCompletion(Lesson completedLesson, Student student,Progress p){
+      p.addCompletedLesson(completedLesson);
+      student.setProgresses(p.getCompletedLessons());
     }
-    public void progressionData(){
+    public void progressionData(Student student){
+        ArrayList<Progress> progresses=new ArrayList(student.getProgresses());
+        for(Progress p:progresses){
+            System.out.println("Course is: "+ p.getCourse().getTitle());
+            System.out.println("Lesson completed: "+p.getCompletedLessons().size());
+            System.out.println("Progress Completed: "+ p.getPercentage());
+            System.out.println("Course Completed? "+ p.courseCompletion());
+        }
+
         
     }
     
